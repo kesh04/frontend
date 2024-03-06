61f7a3f75a9b59c7651a0f89e2bd0d7dd4ff74ed
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2024-03-06 11:04:27 +0530

    Initialize project using Create React App

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,16 @@
+import { useAuthContext } from './useAuthContext'
+import { useWorkoutsContext } from './useWorkoutsContext'
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext()
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext()
+
+  const logout = () => {
+    localStorage.removeItem('user')
+
+    dispatch({ type: 'LOGOUT' })
+    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
+  }
+
+  return { logout }
+}
